ed77d0abc5d040689f971da34cd4d52d587d40e6
TaiefSiddique <taiefsiddik2@gmail.com>
2024-02-26 14:33:51 +0600

    Mongodb database added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 
@@ -9,22 +10,50 @@ const users = [{ id: 1, name: "John Doe", email: "john@example.com" }, { id: 2, 
 
 app.use(cors())
 app.use(express.json());
-app.get('/', (req, res) => {
-    res.send('users management server is running')
-})
+const uri = "mongodb+srv://taiefsiddik2:XGAwejOhZUYdQ1He@cluster0.nebbavw.mongodb.net/?retryWrites=true&w=majority";
 
-app.get('/users', (req, res) => {
-    res.send(users);
-})
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
-app.post('/users', (req, res) => {
-    console.log('post api hitting')
-    console.log(req.body);
-    const newUser = req.body;
-    newUser.id = users.length + 1;
-    users.push(newUser);
-    res.send(newUser);
-})
+async function run() {
+    try {
+        await client.connect();
+        const userData = client.db("insertDB").collection("userData");
+        app.get('/', (req, res) => {
+            res.send('users management server is running')
+        })
+
+        app.get('/users', async (req, res) => {
+            // res.send(users);
+            const cursor = userData.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/users', async (req, res) => {
+            // console.log('post api hitting')
+            // console.log(req.body);
+            // const newUser = req.body;
+            // newUser.id = users.length + 1;
+            // users.push(newUser);
+            // res.send(newUser);
+            const user = req.body;
+            const result = await userData.insertOne(user);
+            res.send(result);
+        })
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        //await client.close();
+    }
+}
+run().catch(console.log);
 
 app.listen(port, () => {
     console.log(`server is running on Port: ${port}`)
